cf3c56fc21151b58ff9311533721b7fb9df07cb8
iteguh08 <iputugedeteguhprawira@gmail.com>
2025-02-25 23:38:33 +0800

    updt3

--- app/Models/product.php
+++ app/Models/product.php
@@ -1,8 +1,4 @@
 <?php
-// Langkah 1: Membuat Model dan Migration sekaligus
-// Command: php artisan make:model Product -m
-
-// File: app/Models/Product.php
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -12,7 +8,6 @@ class Product extends Model
 {
     use HasFactory;
     
-    // Tentukan fields yang dapat diisi
     protected $fillable = [
         'name',
         'description',
@@ -21,7 +16,6 @@ class Product extends Model
     ];
 }
 
-// File: database/migrations/xxxx_xx_xx_xxxxxx_create_products_table.php
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -45,13 +39,3 @@ return new class extends Migration
         Schema::dropIfExists('products');
     }
 };
-
-// Langkah 3: Mendaftarkan Route di web.php
-// File: routes/web.php
-use App\Http\Controllers\ProductController;
-use Illuminate\Support\Facades\Route;
-
-Route::resource('products', ProductController::class);
-
-// Langkah 4: Menjalankan migrasi
-// Command: php artisan migrate
