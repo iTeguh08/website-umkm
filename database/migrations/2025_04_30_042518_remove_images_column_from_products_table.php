55a389e0283764f775ad1c56cbe722df0cfd5f4a
iteguh08 <iputugedeteguhprawira@gmail.com>
2025-04-30 16:29:43 +0800

    redirect saat login

---
+++ database/migrations/2025_04_30_042518_remove_images_column_from_products_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveImagesColumnFromProductsTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('images');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('images')->nullable()->after('telephone');
+        });
+    }
+}
